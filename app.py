5ba2932ef0f21e5cb2890a79197449183e2cc1d6
Sarbsey <ess5393@psu.edu>
2023-07-24 20:33:02 -0400

    cleaning

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from urllib.parse import urlencode
 from util import button
 from util import faunadatafunctions as fdb
 from util import Spotvac_functions as sf
+import time
 
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ Session(app)
 
 @app.route("/", methods=['GET','POST'])
 def index():
+  counter_update()
   return render_template("index.html")
 
 stateKey = 'spotify_auth_state'
@@ -29,8 +31,8 @@ stateKey = 'spotify_auth_state'
 
 @app.route("/request_authorization", methods=['GET','POST'])
 def request_authorization():
+  
   if request.method == "POST":
-
     full_auth_url = sf.request_authorization()
     
   return redirect(full_auth_url, code=302)
@@ -41,6 +43,7 @@ def launch_spotvac(full_user_info):
     '''
     Run SpotVac
     '''
+    
     token = full_user_info['data']['token_data']['access_token']
     client_id = os.environ['client_ID']
     client_secret = os.environ['client_secret']
@@ -48,6 +51,8 @@ def launch_spotvac(full_user_info):
     sp = spotipy.Spotify(client_credentials_manager=manager, auth=token)
 
     sf.run_spotvac(sp, full_user_info)
+    
+
     return redirect("/", code=302)
 
 
@@ -57,15 +62,11 @@ def launch_spotvac(full_user_info):
 def callback():
   token_data = sf.generate_token()
   user_info = sf.generate_user_info(token_data)
-  client_id = os.environ['client_ID']
-  client_secret = os.environ['client_secret']
-  manager = SpotifyClientCredentials(client_id,client_secret)
-  sp = spotipy.Spotify(client_credentials_manager=manager, auth=token_data['access_token'])
-
-
   submit = fdb.format_data(user_info, token_data)
+
   fdb.submit(submit)
-  launch_spotvac(submit)
+  #launch_spotvac(submit)
+
   return redirect("/", code=302)
 
 
@@ -92,9 +93,13 @@ def other_links():
 def dev_log():
   return render_template('construction.html')
 
+@app.route("/legal")
+def legal():
+  return render_template('construction.html')
 
 
 
-'''if __name__ == '__main__':
+
+if __name__ == '__main__':
     app.run(port="5000", debug=True)
-'''
+
--- util/Spotvac_functions.py
+++ util/Spotvac_functions.py
@@ -43,7 +43,7 @@ def request_authorization():
     authorize_url = os.environ['authorize_url']
     client_id = os.environ['client_ID']
     redirect_uri = os.environ['redirect_url']
-    scope = 'playlist-modify-public user-library-read'
+    scope = 'playlist-modify-public user-library-read user-read-private'
 
     auth_dict = {
         'client_id': client_id,
@@ -88,6 +88,7 @@ def generate_token():
 def generate_user_info(token_data):
     client_id = os.environ['client_ID']
     client_secret = os.environ['client_secret']
+
 
     manager = SpotifyClientCredentials(client_id,client_secret)
     sp = spotipy.Spotify(client_credentials_manager=manager, auth=token_data['access_token'])
